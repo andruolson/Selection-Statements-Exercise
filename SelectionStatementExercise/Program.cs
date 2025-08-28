47d3299c63606b18b611f831fc3841f6e5a50b5f
Andrew Olson <andruolson@gmail.com>
2025-08-28 16:10:11 -0600

    Did exercise one, had it loop the code until the user got it right. Had chatgpt explain some of it, don't fully understand the -while- statement, but saw the ! = means !=

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int favoriteNumber = 88;
+            int guess = 0;
+
+            while (guess != favoriteNumber)
+            {
+                Console.WriteLine("Guess my favorite number!");
+                string userInput = (Console.ReadLine());
+                int.TryParse(userInput, out guess);
+
+                if (guess == favoriteNumber)
+                {
+                    Console.WriteLine("You got the favorite number!");
+                }
+                else if (guess > favoriteNumber)
+                {
+                    Console.WriteLine("That's too big! Try again!");
+                }
+                else if (guess < favoriteNumber)
+                {
+                    Console.WriteLine("That's to small! Try again!");
+                }
+                else
+                    Console.WriteLine("You got my favorite number!");
+            }
         }
     }
 }
